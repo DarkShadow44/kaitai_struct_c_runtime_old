1579173f43fa7ebf0c855c65eabd5c3e0798a3bf
Fabian Maurer <dark.shadow4@web.de>
2021-08-29 19:58:45 +0200

    start usertype

--- kaitaistruct.c
+++ kaitaistruct.c
@@ -1,73 +1,65 @@
+#define KS_DEPEND_ON_INTERNALS
 #include "kaitaistruct.h"
 
-typedef struct ks_handle_
-{
-} ks_handle;
-
-typedef struct ks_stream_
-{
-} ks_stream;
-
-typedef struct ks_bytes_
-{
-    uint8_t *data;
-    uint32_t len;
-} ks_bytes;
-
-ks_stream* ks_stream_create_from_file(FILE* file)
-{
-	ks_stream* stream = calloc(1, sizeof(ks_stream));
-
-	return stream;
-}
-
-int ks_stream_read_u1(ks_stream* stream, uint8_t* value, void* ignored1, void* ignored2)
-{
-}
-int ks_stream_read_u2le(ks_stream* stream, uint16_t* value, void* ignored1, void* ignored2)
-{
-}
-int ks_stream_read_u4le(ks_stream* stream, uint32_t* value, void* ignored1, void* ignored2)
-{
-}
-int ks_stream_read_u8le(ks_stream* stream, uint64_t* value, void* ignored1, void* ignored2)
-{
-}
-int ks_stream_read_u2be(ks_stream* stream, uint16_t* value, void* ignored1, void* ignored2)
-{
-}
-int ks_stream_read_u4be(ks_stream* stream, uint32_t* value, void* ignored1, void* ignored2)
-{
-}
-int ks_stream_read_u8be(ks_stream* stream, uint64_t* value, void* ignored1, void* ignored2)
+int ks_stream_create_from_file(ks_stream* stream, FILE* file)
 {
 }
 
-int ks_stream_read_s1(ks_stream* stream, uint8_t* value, void* ignored1, void* ignored2)
-{
-}
-int ks_stream_read_s2le(ks_stream* stream, uint16_t* value, void* ignored1, void* ignored2)
-{
-}
-int ks_stream_read_s4le(ks_stream* stream, uint32_t* value, void* ignored1, void* ignored2)
-{
-}
-int ks_stream_read_s8le(ks_stream* stream, uint64_t* value, void* ignored1, void* ignored2)
-{
-}
-int ks_stream_read_s2be(ks_stream* stream, uint16_t* value, void* ignored1, void* ignored2)
-{
-}
-int ks_stream_read_s4be(ks_stream* stream, uint32_t* value, void* ignored1, void* ignored2)
-{
-}
-int ks_stream_read_s8be(ks_stream* stream, uint64_t* value, void* ignored1, void* ignored2)
+int ks_stream_create_from_bytes(ks_stream* stream, ks_bytes* bytes)
 {
 }
 
-int ks_stream_read_bits_le(int width, ks_stream* stream, uint32_t* value, void* ignored1, void* ignored2)
+int ks_stream_read_u1(ks_stream* stream, uint8_t* value)
 {
 }
-int ks_stream_read_bits_be(int width, ks_stream* stream, uint32_t* value, void* ignored1, void* ignored2)
+int ks_stream_read_u2le(ks_stream* stream, uint16_t* value)
+{
+}
+int ks_stream_read_u4le(ks_stream* stream, uint32_t* value)
+{
+}
+int ks_stream_read_u8le(ks_stream* stream, uint64_t* value)
+{
+}
+int ks_stream_read_u2be(ks_stream* stream, uint16_t* value)
+{
+}
+int ks_stream_read_u4be(ks_stream* stream, uint32_t* value)
+{
+}
+int ks_stream_read_u8be(ks_stream* stream, uint64_t* value)
+{
+}
+
+int ks_stream_read_s1(ks_stream* stream, uint8_t* value)
+{
+}
+int ks_stream_read_s2le(ks_stream* stream, uint16_t* value)
+{
+}
+int ks_stream_read_s4le(ks_stream* stream, uint32_t* value)
+{
+}
+int ks_stream_read_s8le(ks_stream* stream, uint64_t* value)
+{
+}
+int ks_stream_read_s2be(ks_stream* stream, uint16_t* value)
+{
+}
+int ks_stream_read_s4be(ks_stream* stream, uint32_t* value)
+{
+}
+int ks_stream_read_s8be(ks_stream* stream, uint64_t* value)
+{
+}
+
+int ks_stream_read_bits_le(ks_stream* stream, int width, uint32_t* value)
+{
+}
+int ks_stream_read_bits_be(ks_stream* stream, int width, uint32_t* value)
+{
+}
+
+int ks_stream_read_bytes(ks_stream* stream, int len, ks_bytes* bytes)
 {
 }
--- kaitaistruct.h
+++ kaitaistruct.h
@@ -2,6 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef KS_DEPEND_ON_INTERNALS
+typedef struct ks_handle_
+{
+} ks_handle;
+
+typedef struct ks_stream_
+{
+} ks_stream;
+
+typedef struct ks_bytes_
+{
+    uint8_t *data;
+    uint32_t len;
+} ks_bytes;
+#else
+
 struct ks_handle_;
 typedef struct ks_handle_ ks_handle;
 
@@ -10,25 +26,28 @@ typedef struct ks_stream_ ks_stream;
 
 struct ks_bytes_;
 typedef struct ks_bytes_ ks_bytes;
+#endif
 
-ks_stream* ks_stream_create_from_file(FILE* file);
+int ks_stream_create_from_file(ks_stream* stream, FILE* file);
+int ks_stream_create_from_bytes(ks_stream* stream, ks_bytes* bytes);
 
-int ks_stream_read_u1(ks_stream* stream, uint8_t* value, void* ignored1, void* ignored2);
-int ks_stream_read_u2le(ks_stream* stream, uint16_t* value, void* ignored1, void* ignored2);
-int ks_stream_read_u4le(ks_stream* stream, uint32_t* value, void* ignored1, void* ignored2);
-int ks_stream_read_u8le(ks_stream* stream, uint64_t* value, void* ignored1, void* ignored2);
-int ks_stream_read_u2be(ks_stream* stream, uint16_t* value, void* ignored1, void* ignored2);
-int ks_stream_read_u4be(ks_stream* stream, uint32_t* value, void* ignored1, void* ignored2);
-int ks_stream_read_u8be(ks_stream* stream, uint64_t* value, void* ignored1, void* ignored2);
+int ks_stream_read_u1(ks_stream* stream, uint8_t* value);
+int ks_stream_read_u2le(ks_stream* stream, uint16_t* value);
+int ks_stream_read_u4le(ks_stream* stream, uint32_t* value);
+int ks_stream_read_u8le(ks_stream* stream, uint64_t* value);
+int ks_stream_read_u2be(ks_stream* stream, uint16_t* value);
+int ks_stream_read_u4be(ks_stream* stream, uint32_t* value);
+int ks_stream_read_u8be(ks_stream* stream, uint64_t* value);
 
-int ks_stream_read_s1(ks_stream* stream, uint8_t* value, void* ignored1, void* ignored2);
-int ks_stream_read_s2le(ks_stream* stream, uint16_t* value, void* ignored1, void* ignored2);
-int ks_stream_read_s4le(ks_stream* stream, uint32_t* value, void* ignored1, void* ignored2);
-int ks_stream_read_s8le(ks_stream* stream, uint64_t* value, void* ignored1, void* ignored2);
-int ks_stream_read_s2be(ks_stream* stream, uint16_t* value, void* ignored1, void* ignored2);
-int ks_stream_read_s4be(ks_stream* stream, uint32_t* value, void* ignored1, void* ignored2);
-int ks_stream_read_s8be(ks_stream* stream, uint64_t* value, void* ignored1, void* ignored2);
+int ks_stream_read_s1(ks_stream* stream, uint8_t* value);
+int ks_stream_read_s2le(ks_stream* stream, uint16_t* value);
+int ks_stream_read_s4le(ks_stream* stream, uint32_t* value);
+int ks_stream_read_s8le(ks_stream* stream, uint64_t* value);
+int ks_stream_read_s2be(ks_stream* stream, uint16_t* value);
+int ks_stream_read_s4be(ks_stream* stream, uint32_t* value);
+int ks_stream_read_s8be(ks_stream* stream, uint64_t* value);
 
-int ks_stream_read_bits_le(int width, ks_stream* stream, uint32_t* value, void* ignored1, void* ignored2);
-int ks_stream_read_bits_be(int width, ks_stream* stream, uint32_t* value, void* ignored1, void* ignored2);
+int ks_stream_read_bits_le(ks_stream* stream, int width, uint32_t* value);
+int ks_stream_read_bits_be(ks_stream* stream, int width, uint32_t* value);
 
+int ks_stream_read_bytes(ks_stream* stream, int len, ks_bytes* bytes);
